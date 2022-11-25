800f5f559ad53635f26c1cfcf22aa14bbe1c8c0e
mitrikov <j6p@bk.ru>
2022-11-26 05:18:17 +1200

    Ахахахах

--- resources/js/Pages/Organisation.vue
+++ resources/js/Pages/Organisation.vue
@@ -1,7 +1,13 @@
 <template>
     <div class="container">
         <h1>Организация: <span>{{ $page.props.organisation.organisation_name }}</span></h1>
-{{$page.props.organisation}}
+        {{ $page.props.organisation }}
+        <form action="post">
+            <input type="text" name="organisation_name" id="organisation_name">
+            <input type="text" name="address" id="address">
+            <textarea name="description" id="description"></textarea>
+            <input type="submit" value="Изменить организацию">
+        </form>
     </div>
 </template>
 
